fa8c2416b5a69c76bc482d01df5845608e9198b4
Arun Abraham <arun4xp@gmail.com>
2017-07-12 20:57:20 +0530

    restructure + add file path config

--- motion-detection/motion-detect-with-images.py
+++ motion-detection/motion-detect-with-images.py
@@ -1,11 +1,15 @@
 from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
+import yaml
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 camera = PiCamera()
 pir = MotionSensor(4)
 while True:
     pir.wait_for_motion()
-    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.png")
+    filename = datetime.now().strftime(cfg["image-path"] + "%Y-%m-%d_%H.%M.%S.png")
     camera.capture(filename)
     pir.wait_for_no_motion()
