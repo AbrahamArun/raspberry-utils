5a03a57d957057c87eacbcb674dc6a122777a14f
Arun Abraham <arun4xp@gmail.com>
2017-07-12 20:07:45 +0530

    fix var def

--- check-wifi.sh
+++ check-wifi.sh
@@ -1,11 +1,9 @@
 pingip='google.com'
-/bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
-
-# interface to check
 wlan='wlan0'
 LOG_PATH='/home/pi/Desktop/log/network.log'
 now=$(date +"%m-%d %r")
 
+/bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
 if [ $? -ge 1 ] ; then
   echo "No network connection, restarting wlan0"
   echo "$now Network is DOWN. Perform a reset" >> $LOG_PATH
