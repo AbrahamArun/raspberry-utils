db75832e94de7f19e86948cf9470d25389d7e066
Arun Abraham <arun4xp@gmail.com>
2017-07-12 20:46:58 +0530

    fix file ext

--- motion-detect-with-images.py
+++ motion-detect-with-images.py
@@ -6,6 +6,6 @@ camera = PiCamera()
 pir = MotionSensor(4)
 while True:
     pir.wait_for_motion()
-    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.png")
     camera.capture(filename)
     pir.wait_for_no_motion()
