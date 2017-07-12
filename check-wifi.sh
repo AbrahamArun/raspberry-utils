14b88102c48694b23f48def185abcffd456305c2
Arun Abraham <arun4xp@gmail.com>
2017-07-12 19:46:50 +0530

    add log

--- check-wifi.sh
+++ check-wifi.sh
@@ -1,9 +1,16 @@
 ping -c4 192.168.1.1 > /dev/null
 
+# interface to check
+wlan='wlan0'
+LOG_PATH='/home/pi/Desktop/log/network.log'
+
 if [ $? != 0 ]
 then
   echo "No network connection, restarting wlan0"
-  /sbin/ifdown 'wlan0'
+  echo "$now Network is DOWN. Perform a reset" >> $LOG_PATH
+  /sbin/ifdown $wlan
   sleep 5
-  /sbin/ifup --force 'wlan0'
+  /sbin/ifup --force $wlan
+else
+  echo "$now Network is UP." >> $LOG_PATH
 fi
